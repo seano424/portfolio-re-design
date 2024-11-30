c3ea155c59560afc7f20c12462257f83e894f161
seano424 <soreilly424@gmail.com>
2024-11-30 13:05:18 +0800

    image grid links

--- components/sections/ImageGrid.tsx
+++ components/sections/ImageGrid.tsx
@@ -1,10 +1,22 @@
 import clsx from 'clsx'
 import Image from 'next/image'
 
-const images = [
-	'/images/projects/voyage/voyage-2.jpeg',
-	'/images/projects/kmb/kmb-2.jpeg',
-	'/images/projects/ftlwd/ftlwd-2.jpeg',
+const links = [
+	{
+		image: '/images/projects/voyage/voyage-2.jpeg',
+		title: 'Voyage Away',
+		link: 'https://voyage-away-subscription-starter.vercel.app/',
+	},
+	{
+		image: '/images/projects/kmb/kmb-2.jpeg',
+		title: 'KMB',
+		link: 'https://www.karriemariebaxley.com/',
+	},
+	{
+		image: '/images/projects/ftlwd/ftlwd-2.jpeg',
+		title: 'FTLWD',
+		link: 'https://www.fromthelodgewithdaniel.com/',
+	},
 ]
 
 const ImageGrid = () => {
@@ -14,20 +26,23 @@ const ImageGrid = () => {
 				Here’s a few things I’m involved in right now:
 			</h2>
 			<div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
-				{images.map((image, index) => (
-					<div
+				{links.map(({ image, title, link }, index) => (
+					<a
+						rel="noopener noreferrer"
+						target="_blank"
 						key={index}
+						href={link}
 						className="relative h-[400px] w-full lg:aspect-square"
 					>
 						<Image
 							fill
 							priority
 							src={image}
-							alt="Image"
+							alt={title}
 							className="rounded-2xl object-cover object-top"
 							sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
 						/>
-					</div>
+					</a>
 				))}
 			</div>
 		</section>
