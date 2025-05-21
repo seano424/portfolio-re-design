21e7e671688e2de0d87ef17e7461fc58fbe3d1f6
seano424 <soreilly424@gmail.com>
2025-05-21 09:30:28 -0700

    Update heading text in ImageGrid component to use proper apostrophe for improved clarity and consistency.

--- components/sections/ImageGrid.tsx
+++ components/sections/ImageGrid.tsx
@@ -9,7 +9,7 @@ const ImageGrid = ({ projects }: Props) => {
 	return (
 		<section className="container">
 			<h2 className="mb-8 text-center text-3xl font-black sm:text-6xl dark:text-white">
-				Apps/Websites I've Built
+				Apps/Websites I&apos;ve Built
 			</h2>
 			<div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
 				{projects &&
