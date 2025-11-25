096472dc8193c47396fcb387511d8c6475a907df
seano424 <soreilly424@gmail.com>
2025-11-24 19:14:42 -0700

    Remove project fetching from Home component and simplify ShowcaseSection usage for cleaner layout.

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
-import { client } from '@/sanity/lib/client'
-import type { Project } from '@/sanity.types'
 import HeroSection from '@/components/sections/HeroSection'
 import ShowcaseSection from '@/components/sections/ShowcaseSection'
-import { getMostRecentProjectsQuery } from '@/sanity/lib/queries'
 
 export default async function Home() {
-	const projects = await client.fetch<Project[]>(getMostRecentProjectsQuery)
-
 	return (
 		<>
 			<HeroSection />
-			<ShowcaseSection projects={projects} />
+			<ShowcaseSection />
 		</>
 	)
 }
